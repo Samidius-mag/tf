f93f7aa3147421b22140ed684bdb430bf39b5bd7
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-26 19:17:58 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
+const { spawn } = require('child_process');
 
+const loadPrice = () => {
+  const child = spawn('node', ['load.js']);
+
+  child.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  child.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+};
+
+const sendToBot = () => {
+  const child = spawn('node', ['bot.js']);
+
+  child.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  child.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+};
+/*
+const sendToBot2 = () => {
+  const child = spawn('node', ['bot2.js']);
+
+  child.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  child.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+};
+*/
+/*
+const sendToBot3 = () => {
+  const child = spawn('node', ['bot3.js']);
+
+  child.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  child.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+};
+*/
+setInterval(loadPrice, 5000);
+setInterval(sendToBot, 300000);
+//setInterval(sendToBot2, 300000);
+//setInterval(sendToBot3, 56000);
