b63b555611de6591a803e5c9e66eab27597f3413
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-26 18:54:44 +1000

    Update tflow2.js

--- tflow2.js
+++ tflow2.js
@@ -36,7 +36,7 @@ model.add(tf.layers.dense({ units: 1, activation: 'linear' }));
 model.compile({ optimizer: 'adam', loss: 'meanSquaredError' });
 
 // Обучение модели
-const epochs = 200;
+const epochs = 300;
 const batchSize = 128;
 
 model.fit(inputTensor, outputTensor, { epochs, batchSize })
