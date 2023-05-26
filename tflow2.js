348f24291dcd784a8d098f6a9a55a5d1fb88e569
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-26 19:01:43 +1000

    Update tflow2.js

--- tflow2.js
+++ tflow2.js
@@ -31,13 +31,13 @@ const outputTensor = tf.tensor1d(output);
 
 // Создание модели
 const model = tf.sequential();
-model.add(tf.layers.dense({ units: 32, inputShape: [windowSize], activation: 'relu' }));
+model.add(tf.layers.dense({ units: 512, inputShape: [windowSize], activation: 'relu' }));
 model.add(tf.layers.dense({ units: 1, activation: 'linear' }));
 model.compile({ optimizer: 'adam', loss: 'meanSquaredError' });
 
 // Обучение модели
 const epochs = 200;
-const batchSize = 16;
+const batchSize = 256;
 
 model.fit(inputTensor, outputTensor, { epochs, batchSize })
   .then(() => {
