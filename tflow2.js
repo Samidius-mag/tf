1233774a9b5b222762ae77b50f12185aaa64dfdf
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-26 17:09:40 +1000

    Update tflow2.js

--- tflow2.js
+++ tflow2.js
@@ -1,16 +1,6 @@
-const fs = require('fs');
 const tf = require('@tensorflow/tfjs');
+const fs = require('fs');
 
-const cont = JSON.parse(fs.readFileSync('cont.json'));
-const weights = cont.weights.map(w => tf.tensor(w));
-
-// Создаем модель нейросети на основе данных из контейнера
-const model = tf.sequential();
-model.add(tf.layers.dense({ units: 4, inputShape: [4] }));
-model.add(tf.layers.dense({ units: 1 }));
-model.setWeights(cont.weights.map(w => tf.tensor(w)));
-
-// Получаем данные о последней свече
 const rawData = fs.readFileSync('price.json');
 const data = JSON.parse(rawData).map(candle => ({
   open: parseFloat(candle.open),
@@ -19,68 +9,114 @@ const data = JSON.parse(rawData).map(candle => ({
   close: parseFloat(candle.close),
   volume: parseFloat(candle.volume),
 }));
-const lastCandle = data[data.length - 1];
 
-// Создаем функцию для прогнозирования цены через указанное количество часов
-function predictPrice(hours) {
-  const input = [lastCandle.open, lastCandle.high, lastCandle.low, lastCandle.close];
-  let output = model.predict(tf.tensor2d([input])).dataSync()[0];
-  for (let i = 0; i < hours; i++) {
-    input.shift();
-    input.push(output);
-    output = model.predict(tf.tensor2d([input])).dataSync()[0];
-  }
-  return output;
+// Подготовка данных
+const prices = data.map(candle => candle.close);
+const minPrice = Math.min(...prices);
+const maxPrice = Math.max(...prices);
+const normalizedPrices = prices.map(price => (price - minPrice) / (maxPrice - minPrice));
+
+const windowSize = 24;
+const input = [];
+const output = [];
+
+for (let i = 0; i < normalizedPrices.length - windowSize; i++) {
+  const window = normalizedPrices.slice(i, i + windowSize);
+  input.push(window);
+  output.push(normalizedPrices[i + windowSize]);
 }
 
-// Создаем функцию для прогнозирования тренда на указанное количество часов
-function predictTrend(hours) {
-  const price1 = predictPrice(hours);
-  const price2 = predictPrice(hours * 2);
-  return price1 < price2 ? 'up' : 'down';
-}
+const inputTensor = tf.tensor2d(input);
+const outputTensor = tf.tensor1d(output);
 
-// Создаем функцию для прогнозирования точек разворота на указанное количество часов
-function predictTurningPoint(hours) {
-  const price1 = predictPrice(hours);
-  const price2 = predictPrice(hours * 2);
-  const price3 = predictPrice(hours * 3);
-  if (price1 < price2 && price2 > price3) {
-    return 'top';
-  } else if (price1 > price2 && price2 < price3) {
-    return 'bottom';
-  } else {
-    return 'none';
-  }
-}
+// Создание модели
+const model = tf.sequential();
+model.add(tf.layers.dense({ units: 64, inputShape: [windowSize], activation: 'relu' }));
+model.add(tf.layers.dense({ units: 1, activation: 'linear' }));
+model.compile({ optimizer: 'adam', loss: 'meanSquaredError' });
 
-// Создаем функцию для прогнозирования уровней сопротивления и поддержки на указанное количество часов
-function predictSupportResistance(hours) {
-  const price1 = predictPrice(hours);
-  const price2 = predictPrice(hours * 2);
-  const price3 = predictPrice(hours * 3);
-  const price4 = predictPrice(hours * 4);
-  const resistance1 = (price1 + price2 + price3) / 3;
-  const resistance2 = (price2 + price3 + price4) / 3;
-  const support1 = (price1 + price2) / 2;
-  const support2 = (price3 + price4) / 2;
-  return { resistance1, resistance2, support1, support2 };
-}
+// Обучение модели
+const epochs = 100;
+const batchSize = 32;
 
-// Выводим результаты прогнозирования
-console.log('Price after 1 hour:', predictPrice(1));
-console.log('Price after 4 hours:', predictPrice(4));
-console.log('Price after 12 hours:', predictPrice(12));
-console.log('Price after 24 hours:', predictPrice(24));
-console.log('Trend after 1 hour:', predictTrend(1));
-console.log('Trend after 4 hours:', predictTrend(4));
-console.log('Trend after 12 hours:', predictTrend(12));
-console.log('Trend after 24 hours:', predictTrend(24));
-console.log('Turning point after 1 hour:', predictTurningPoint(1));
-console.log('Turning point after 4 hours:', predictTurningPoint(4));
-console.log('Turning point after 12 hours:', predictTurningPoint(12));
-console.log('Turning point after 24 hours:', predictTurningPoint(24));
-console.log('Support and resistance after 1 hour:', predictSupportResistance(1));
-console.log('Support and resistance after 4 hours:', predictSupportResistance(4));
-console.log('Support and resistance after 12 hours:', predictSupportResistance(12));
-console.log('Support and resistance after 24 hours:', predictSupportResistance(24));
+model.fit(inputTensor, outputTensor, { epochs, batchSize })
+  .then(() => {
+    // Прогнозы цены
+    const predict = (input, hours) => {
+      const normalizedInput = input.map(price => (price - minPrice) / (maxPrice - minPrice));
+      const window = normalizedInput.slice(-windowSize);
+      const inputTensor = tf.tensor2d([window]);
+      const predictedNormalizedPrice = model.predict(inputTensor).dataSync()[0];
+      const predictedPrice = predictedNormalizedPrice * (maxPrice - minPrice) + minPrice;
+      return predictedPrice * Math.pow(1.01, hours);
+    };
+
+    console.log('Price predictions:');
+    console.log(`1 hour: ${predict(prices.slice(-windowSize), 1)}`);
+    console.log(`4 hours: ${predict(prices.slice(-windowSize), 4)}`);
+    console.log(`12 hours: ${predict(prices.slice(-windowSize), 12)}`);
+    console.log(`24 hours: ${predict(prices.slice(-windowSize), 24)}`);
+
+    // Прогнозы тренда
+    const trend = (input, hours) => {
+      const normalizedInput = input.map(price => (price - minPrice) / (maxPrice - minPrice));
+      const window = normalizedInput.slice(-windowSize);
+      const inputTensor = tf.tensor2d([window]);
+      const predictedNormalizedPrice = model.predict(inputTensor).dataSync()[0];
+      const predictedPrice = predictedNormalizedPrice * (maxPrice - minPrice) + minPrice;
+      return predictedPrice > input[input.length - 1] ? 'up' : 'down';
+    };
+
+    console.log('Trend predictions:');
+    console.log(`1 hour: ${trend(prices.slice(-windowSize), 1)}`);
+    console.log(`4 hours: ${trend(prices.slice(-windowSize), 4)}`);
+    console.log(`12 hours: ${trend(prices.slice(-windowSize), 12)}`);
+    console.log(`24 hours: ${trend(prices.slice(-windowSize), 24)}`);
+
+    // Прогнозы точек разворота
+    const reversalPoints = (input, hours) => {
+      const normalizedInput = input.map(price => (price - minPrice) / (maxPrice - minPrice));
+      const window = normalizedInput.slice(-windowSize);
+      const inputTensor = tf.tensor2d([window]);
+      const predictedNormalizedPrice = model.predict(inputTensor).dataSync()[0];
+      const predictedPrice = predictedNormalizedPrice * (maxPrice - minPrice) + minPrice;
+      const currentPrice = input[input.length - 1];
+      const threshold = 0.01;
+      if (predictedPrice > currentPrice * (1 + threshold)) {
+        return 'up';
+      } else if (predictedPrice < currentPrice * (1 - threshold)) {
+        return 'down';
+      } else {
+        return 'none';
+      }
+    };
+
+    console.log('Reversal point predictions:');
+    console.log(`1 hour: ${reversalPoints(prices.slice(-windowSize), 1)}`);
+    console.log(`4 hours: ${reversalPoints(prices.slice(-windowSize), 4)}`);
+    console.log(`12 hours: ${reversalPoints(prices.slice(-windowSize), 12)}`);
+    console.log(`24 hours: ${reversalPoints(prices.slice(-windowSize), 24)}`);
+
+    // Уровни сопротивления и поддержки
+    const supportResistanceLevels = (input, hours) => {
+      const normalizedInput = input.map(price => (price - minPrice) / (maxPrice - minPrice));
+      const window = normalizedInput.slice(-windowSize);
+      const inputTensor = tf.tensor2d([window]);
+      const predictedNormalizedPrice = model.predict(inputTensor).dataSync()[0];
+      const predictedPrice = predictedNormalizedPrice * (maxPrice - minPrice) + minPrice;
+      const currentPrice = input[input.length - 1];
+      const threshold = 0.01;
+      const supportLevel = currentPrice * (1 - threshold);
+      const resistanceLevel = currentPrice * (1 + threshold);
+      return { supportLevel, resistanceLevel };
+    };
+
+    console.log('Support and resistance levels:');
+    console.log(`1 hour: ${JSON.stringify(supportResistanceLevels(prices.slice(-windowSize), 1))}`);
+    console.log(`4 hours: ${JSON.stringify(supportResistanceLevels(prices.slice(-windowSize), 4))}`);
+    console.log(`12 hours: ${JSON.stringify(supportResistanceLevels(prices.slice(-windowSize), 12))}`);
+    console.log(`24 hours: ${JSON.stringify(supportResistanceLevels(prices.slice(-windowSize), 24))}`);
+  })
+  .catch(error => {
+    console.error(error);
+  });
